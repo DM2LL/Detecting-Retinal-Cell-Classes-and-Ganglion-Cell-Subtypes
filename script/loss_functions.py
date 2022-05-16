251e537c829801e7572eeb75add56bc0a035fbeb
Data Mining Machine Learning <35179314+DM2LL@users.noreply.github.com>
2022-05-16 11:02:59 -0500

    Add files via upload

---
+++ script/loss_functions.py
@@ -0,0 +1,255 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+cudnn.deterministic = True
+cudnn.benchmark = True
+import random
+seed=666
+random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+np.random.seed(seed)
+import contextlib
+import torch.nn.functional as F
+
+
+
+############# VAT Loss
+@contextlib.contextmanager
+def _disable_tracking_bn_stats(model):
+    def switch_attr(m):
+        if hasattr(m, 'track_running_stats'):
+            m.track_running_stats ^= True
+    model.apply(switch_attr)
+    yield
+    model.apply(switch_attr)
+
+def _l2_normalize(d):
+    d_reshaped = d.view(d.shape[0], -1, *(1 for _ in range(d.dim() - 2)))
+    d /= torch.norm(d_reshaped, dim=1, keepdim=True) + 1e-8
+    return d
+
+class VATLoss(nn.Module):
+    def __init__(self, xi=10.0, eps=1.0, ip=1):
+        """VAT loss
+        :param xi: hyperparameter of VAT (default: 10.0)
+        :param eps: hyperparameter of VAT (default: 1.0)
+        :param ip: iteration times of computing adv noise (default: 1)
+        """
+        super(VATLoss, self).__init__()
+        self.xi = xi
+        self.eps = eps
+        self.ip = ip
+    def forward(self, model, x):
+        with torch.no_grad():
+            pred = F.softmax(model(x), dim=1)
+        # prepare random unit tensor
+        d = torch.rand(x.shape).sub(0.5).to(x.device)
+        d = _l2_normalize(d)
+        with _disable_tracking_bn_stats(model):
+            # calc adversarial direction
+            for _ in range(self.ip):
+                d.requires_grad_()
+                pred_hat = model(x + self.xi * d)
+                logp_hat = F.log_softmax(pred_hat, dim=1)
+                adv_distance = F.kl_div(logp_hat, pred, reduction='batchmean')
+                adv_distance.backward()
+                d = _l2_normalize(d.grad)
+                model.zero_grad()
+            # calc LDS
+            r_adv = d * self.eps
+            pred_hat = model(x + r_adv)
+            logp_hat = F.log_softmax(pred_hat, dim=1)
+            lds = F.kl_div(logp_hat, pred, reduction='batchmean')
+        return lds
+        
+
+def BCELossForMultiClassification(label, predict_prob, class_level_weight=None, instance_level_weight=None, epsilon=1e-12):
+    N, C = label.size()
+    N_, C_ = predict_prob.size()
+    assert N == N_ and C == C_, 'fatal error: dimension mismatch!'
+    if class_level_weight is None:
+        class_level_weight = 1.0
+    else:
+        if len(class_level_weight.size()) == 1:
+            class_level_weight = class_level_weight.view(1, class_level_weight.size(0))
+        assert class_level_weight.size(1) == C, 'fatal error: dimension mismatch!'
+    if instance_level_weight is None:
+        instance_level_weight = 1.0
+    else:
+        if len(instance_level_weight.size()) == 1:
+            instance_level_weight = instance_level_weight.view(instance_level_weight.size(0), 1)
+        assert instance_level_weight.size(0) == N, 'fatal error: dimension mismatch!'
+    bce = -label * torch.log(predict_prob + epsilon) - (1.0 - label) * torch.log(1.0 - predict_prob + epsilon)
+    return torch.sum(instance_level_weight * bce * class_level_weight) / float(N)
+
+
+def CrossEntropyLoss(label, predict_prob, class_level_weight=None, instance_level_weight=None, epsilon=1e-12):
+    N, C = label.size()
+    N_, C_ = predict_prob.size()
+    assert N == N_ and C == C_, 'fatal error: dimension mismatch!'
+    if class_level_weight is None:
+        class_level_weight = 1.0
+    else:
+        if len(class_level_weight.size()) == 1:
+            class_level_weight = class_level_weight.view(1, class_level_weight.size(0))
+        assert class_level_weight.size(1) == C, 'fatal error: dimension mismatch!'
+    if instance_level_weight is None:
+        instance_level_weight = 1.0
+    else:
+        if len(instance_level_weight.size()) == 1:
+            instance_level_weight = instance_level_weight.view(instance_level_weight.size(0), 1)
+        assert instance_level_weight.size(0) == N, 'fatal error: dimension mismatch!'
+    ce = -label * torch.log(predict_prob + epsilon)
+    return torch.sum(instance_level_weight * ce * class_level_weight) / float(N)
+
+
+#############  CMMD Loss
+min_var_est = 1e-8
+
+# Consider linear time MMD with a linear kernel:
+def linear_mmd2(f_of_X, f_of_Y):
+    loss = 0.0
+    delta = f_of_X - f_of_Y
+    loss = torch.mean((delta[:-1] * delta[1:]).sum(1))
+    return loss
+
+
+# Consider linear time MMD with a polynomial kernel:
+def poly_mmd2(f_of_X, f_of_Y, d=2, alpha=1.0, c=2.0):
+    K_XX = (alpha * (f_of_X[:-1] * f_of_X[1:]).sum(1) + c)
+    K_XX_mean = torch.mean(K_XX.pow(d))
+
+    K_YY = (alpha * (f_of_Y[:-1] * f_of_Y[1:]).sum(1) + c)
+    K_YY_mean = torch.mean(K_YY.pow(d))
+
+    K_XY = (alpha * (f_of_X[:-1] * f_of_Y[1:]).sum(1) + c)
+    K_XY_mean = torch.mean(K_XY.pow(d))
+
+    K_YX = (alpha * (f_of_Y[:-1] * f_of_X[1:]).sum(1) + c)
+    K_YX_mean = torch.mean(K_YX.pow(d))
+
+    return K_XX_mean + K_YY_mean - K_XY_mean - K_YX_mean
+
+
+def _mix_rbf_kernel(X, Y, sigma_list):
+    assert(X.size(0) == Y.size(0))
+    m = X.size(0)
+    Z = torch.cat((X, Y), 0)
+    ZZT = torch.mm(Z, Z.t())
+    diag_ZZT = torch.diag(ZZT).unsqueeze(1)
+    Z_norm_sqr = diag_ZZT.expand_as(ZZT)
+    exponent = Z_norm_sqr - 2 * ZZT + Z_norm_sqr.t()
+    K = 0.0
+    for sigma in sigma_list:
+        gamma = 1.0 / (2 * sigma**2)
+        K += torch.exp(-gamma * exponent)
+    return K[:m, :m], K[:m, m:], K[m:, m:], len(sigma_list)
+
+
+def mix_rbf_mmd2(X, Y, sigma_list, biased=True):
+    K_XX, K_XY, K_YY, d = _mix_rbf_kernel(X, Y, sigma_list)
+    # return _mmd2(K_XX, K_XY, K_YY, const_diagonal=d, biased=biased)
+    return _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=biased)
+
+
+def mix_rbf_mmd2_and_ratio(X, Y, sigma_list, biased=True):
+    K_XX, K_XY, K_YY, d = _mix_rbf_kernel(X, Y, sigma_list)
+    # return _mmd2_and_ratio(K_XX, K_XY, K_YY, const_diagonal=d, biased=biased)
+    return _mmd2_and_ratio(K_XX, K_XY, K_YY, const_diagonal=False, biased=biased)
+
+
+
+###### functions to compute variances based on kernel matrices
+
+def _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    m = K_XX.size(0)    # assume X, Y are same shape
+
+    # Get the various sums of kernels that we'll use
+    # Kts drop the diagonal, but we don't need to compute them explicitly
+    if const_diagonal is not False:
+        diag_X = diag_Y = const_diagonal
+        sum_diag_X = sum_diag_Y = m * const_diagonal
+    else:
+        diag_X = torch.diag(K_XX)                       # (m,)
+        diag_Y = torch.diag(K_YY)                       # (m,)
+        sum_diag_X = torch.sum(diag_X)
+        sum_diag_Y = torch.sum(diag_Y)
+
+    Kt_XX_sums = K_XX.sum(dim=1) - diag_X             # \tilde{K}_XX * e = K_XX * e - diag_X
+    Kt_YY_sums = K_YY.sum(dim=1) - diag_Y             # \tilde{K}_YY * e = K_YY * e - diag_Y
+    K_XY_sums_0 = K_XY.sum(dim=0)                     # K_{XY}^T * e
+
+    Kt_XX_sum = Kt_XX_sums.sum()                       # e^T * \tilde{K}_XX * e
+    Kt_YY_sum = Kt_YY_sums.sum()                       # e^T * \tilde{K}_YY * e
+    K_XY_sum = K_XY_sums_0.sum()                       # e^T * K_{XY} * e
+
+    if biased:
+        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
+            + (Kt_YY_sum + sum_diag_Y) / (m * m)
+            - 2.0 * K_XY_sum / (m * m))
+    else:
+        mmd2 = (Kt_XX_sum / (m * (m - 1))
+            + Kt_YY_sum / (m * (m - 1))
+            - 2.0 * K_XY_sum / (m * m))
+
+    return mmd2
+
+
+def _mmd2_and_ratio(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    mmd2, var_est = _mmd2_and_variance(K_XX, K_XY, K_YY, const_diagonal=const_diagonal, biased=biased)
+    loss = mmd2 / torch.sqrt(torch.clamp(var_est, min=min_var_est))
+    return loss, mmd2, var_est
+
+
+def _mmd2_and_variance(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    m = K_XX.size(0)    # assume X, Y are same shape
+
+    # Get the various sums of kernels that we'll use
+    # Kts drop the diagonal, but we don't need to compute them explicitly
+    if const_diagonal is not False:
+        diag_X = diag_Y = const_diagonal
+        sum_diag_X = sum_diag_Y = m * const_diagonal
+        sum_diag2_X = sum_diag2_Y = m * const_diagonal**2
+    else:
+        diag_X = torch.diag(K_XX)                       # (m,)
+        diag_Y = torch.diag(K_YY)                       # (m,)
+        sum_diag_X = torch.sum(diag_X)
+        sum_diag_Y = torch.sum(diag_Y)
+        sum_diag2_X = diag_X.dot(diag_X)
+        sum_diag2_Y = diag_Y.dot(diag_Y)
+
+    Kt_XX_sums = K_XX.sum(dim=1) - diag_X             # \tilde{K}_XX * e = K_XX * e - diag_X
+    Kt_YY_sums = K_YY.sum(dim=1) - diag_Y             # \tilde{K}_YY * e = K_YY * e - diag_Y
+    K_XY_sums_0 = K_XY.sum(dim=0)                     # K_{XY}^T * e
+    K_XY_sums_1 = K_XY.sum(dim=1)                     # K_{XY} * e
+
+    Kt_XX_sum = Kt_XX_sums.sum()                       # e^T * \tilde{K}_XX * e
+    Kt_YY_sum = Kt_YY_sums.sum()                       # e^T * \tilde{K}_YY * e
+    K_XY_sum = K_XY_sums_0.sum()                       # e^T * K_{XY} * e
+
+    Kt_XX_2_sum = (K_XX ** 2).sum() - sum_diag2_X      # \| \tilde{K}_XX \|_F^2
+    Kt_YY_2_sum = (K_YY ** 2).sum() - sum_diag2_Y      # \| \tilde{K}_YY \|_F^2
+    K_XY_2_sum  = (K_XY ** 2).sum()                    # \| K_{XY} \|_F^2
+
+    if biased:
+        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
+            + (Kt_YY_sum + sum_diag_Y) / (m * m)
+            - 2.0 * K_XY_sum / (m * m))
+    else:
+        mmd2 = (Kt_XX_sum / (m * (m - 1))
+            + Kt_YY_sum / (m * (m - 1))
+            - 2.0 * K_XY_sum / (m * m))
+
+    var_est = (
+        2.0 / (m**2 * (m - 1.0)**2) * (2 * Kt_XX_sums.dot(Kt_XX_sums) - Kt_XX_2_sum + 2 * Kt_YY_sums.dot(Kt_YY_sums) - Kt_YY_2_sum)
+        - (4.0*m - 6.0) / (m**3 * (m - 1.0)**3) * (Kt_XX_sum**2 + Kt_YY_sum**2)
+        + 4.0*(m - 2.0) / (m**3 * (m - 1.0)**2) * (K_XY_sums_1.dot(K_XY_sums_1) + K_XY_sums_0.dot(K_XY_sums_0))
+        - 4.0*(m - 3.0) / (m**3 * (m - 1.0)**2) * (K_XY_2_sum) - (8 * m - 12) / (m**5 * (m - 1)) * K_XY_sum**2
+        + 8.0 / (m**3 * (m - 1.0)) * (
+            1.0 / m * (Kt_XX_sum + Kt_YY_sum) * K_XY_sum
+            - Kt_XX_sums.dot(K_XY_sums_1)
+            - Kt_YY_sums.dot(K_XY_sums_0))
+        )
+    return mmd2, var_est
